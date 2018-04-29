22e4b7dd4b304f41732a38800bc58d0f074ff82b
ygefen <yoni.gefen@gmail.com>
2018-04-29 15:18:16 -0400

    typo removed

--- example_usage.py
+++ example_usage.py
@@ -6,7 +6,7 @@ from ez_logging import ez_logging
 ez_logging.set_logging()
 
 # Instantiate a simplified handler for DynamoDB, one handler per table.
-plant_status_handle = EZDynamoDBTable("Urban_Garden_Plant_Status", "schemas/Urban_Garden_Plant_Status.json")
+plant_status_handle = EZDynamoDBTable("Urban_Garden_Plant_Status", "templates/Urban_Garden_Plant_Status.json")
 
 # Create data entry and format in a dictionary. Limited validation; keys should match the DB and values should be valid.
 data = {'PlantID': 'ABC123', 'Timestamp': datetime.datetime.today().isoformat(), 'Sensors': {'Humidity': 50}}
